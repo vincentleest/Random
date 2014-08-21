8c35b7ee6941eb911075a49352654534f412340f
Vincent <vincentleest@gmail.com>
2014-08-21 17:37:29 -0400

    Cleaned up scripts

--- HKEX/nok.rb
+++ HKEX/nok.rb
@@ -1,26 +1,13 @@
 #!/usr/bin/env ruby
 
-require 'nokogiri'
-require 'open-uri'
-
-#doc = Nokogiri::HTML(open("http://www.hkex.com.hk/eng/ddp/Contract_Details.asp?PId=2"))
-
-#doc.css('script, link').each { |node| node.remove }
-#puts doc.css('body').text.squeeze(" \n")
-
-
+puts ARGV[0]
 
 fh = File.readlines('HSI.txt')
-
 index_name = fh.first
-
 data = fh.select { |line| line =~ /^(P||C) / }
 data.each { |line| line.gsub!(" ", ",")} 
 
 put_data, call_data = data.partition { |line| line =~ /^P,/}
-
-#puts (call_data.join("\n"))
-#puts (put_data.join("\n"))
 
 File.open("HSI.csv", "w") do |file|
 
