d9cc304b0b7055de0935d21a74817db4a20f948f
vincent <vincentleest@gmail.com>
2014-09-30 17:52:47 -0600

    Modified fetchdata.sh

--- HKEX/fetchData.sh
+++ HKEX/fetchData.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+SCRIPT_ROOT="/home/vincent/workspace/Random/HKEX/"
+DROPBOX_UPLOADER="$SCRIPT_ROOT/dropbox_uploader.sh"
 NOW=$(date +"%F")
+
+cd $SCRIPT_ROOT
 echo "Fetching Data"
 lynx -dump http://www.hkex.com.hk/eng/ddp/Contract_Details.asp?PId=2 | sed -e '1,21d' -e 's/[ \t]*//' > HSI.txt
 lynx -dump http://www.hkex.com.hk/eng/ddp/Contract_Details.asp?PId=6 |
@@ -12,8 +16,13 @@ echo "Data Fetch Completed"
 ./format.rb "HHI"
 
 echo "Finisehd Formatting"
+
+echo "Uploading files"
 #moving files around
-mkdir $NOW
-mv HHI.csv ./$NOW/HHI.csv
-mv HSI.csv ./$NOW/HSI.csv
-rm *.txt
+$DROPBOX_UPLOADER -f /home/vincent/.dropbox_uploader mkdir "$NOW" >> ~/log
+$DROPBOX_UPLOADER -f /home/vincent/.dropbox_uploader upload "$SCRIPT_ROOT/HHI.csv" "$NOW/HHI.csv" >> ~/log
+$DROPBOX_UPLOADER -f /home/vincent/.dropbox_uploader upload "$SCRIPT_ROOT/HSI.csv" "$NOW/HSI.csv" >> ~/log
+
+echo "Upload completed, cleaning up"
+rm HHI.*
+rm HSI.*
