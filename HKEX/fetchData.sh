03724f3c33dc60bc160c1ffec6cc5677c2d33d01
vincent <vincentleest@gmail.com>
2014-09-30 21:41:26 -0600

    Added other stocks

--- HKEX/fetchData.sh
+++ HKEX/fetchData.sh
@@ -11,25 +11,40 @@ lynx -dump http://www.hkex.com.hk/eng/ddp/contract_details.asp?pid=6 |
 sed -e '1,21d' -e 's/[ \t]*//' > HHI.txt
 lynx -dump http://www.hkex.com.hk/eng/ddp/contract_details.asp?pid=117 |
 sed -e '1,21d' -e 's/[ \t]*//' > HKB.txt
+lynx -dump http://www.hkex.com.hk/eng/ddp/contract_details.asp?pid=157 |
+sed -e '1,21d' -e 's/[ \t]*//' > TCH.txt
+lynx -dump http://www.hkex.com.hk/eng/ddp/contract_details.asp?pid=82 |
+sed -e '1,21d' -e 's/[ \t]*//' > HEX.txt
+lynx -dump http://www.hkex.com.hk/eng/ddp/contract_details.asp?pid=63 |
+sed -e '1,21d' -e 's/[ \t]*//' > CHT.txt
 
 echo "Data Fetch Completed"
 
 ./format.rb "HSI"
 ./format.rb "HHI"
 ./format.rb "HKB"
+./format.rb "TCH"
+./format.rb "HEX"
+./format.rb "CHT"
 
 echo "Finisehd Formatting"
 
 echo "Uploading files"
 #moving files around
 $DROPBOX_UPLOADER -f /home/vincent/.dropbox_uploader mkdir "$NOW" >> ~/log
+
 $DROPBOX_UPLOADER -f /home/vincent/.dropbox_uploader upload "$SCRIPT_ROOT/HHI.csv" "$NOW/HHI.csv" >> ~/log
+
 $DROPBOX_UPLOADER -f /home/vincent/.dropbox_uploader upload "$SCRIPT_ROOT/HSI.csv" "$NOW/HSI.csv" >> ~/log
 
 $DROPBOX_UPLOADER -f /home/vincent/.dropbox_uploader upload "$SCRIPT_ROOT/HKB.csv" "$NOW/HKB.csv" >> ~/log
 
+$DROPBOX_UPLOADER -f /home/vincent/.dropbox_uploader upload "$SCRIPT_ROOT/TCH.csv" "$NOW/TCH.csv" >> ~/log
+
+$DROPBOX_UPLOADER -f /home/vincent/.dropbox_uploader upload "$SCRIPT_ROOT/HEX.csv" "$NOW/HEX.csv" >> ~/log
+
+$DROPBOX_UPLOADER -f /home/vincent/.dropbox_uploader upload "$SCRIPT_ROOT/CHT.csv" "$NOW/CHT.csv" >> ~/log
+
 echo "Upload completed, cleaning up"
-#rm *.csv
-rm HHI.*
-rm HSI.*
-rm HKB.*
+rm *.csv
+rm *.txt
