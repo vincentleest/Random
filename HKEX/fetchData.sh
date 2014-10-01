192c25e62700cac660265c2a1fba6dc7542a9401
vincent <vincentleest@gmail.com>
2014-09-30 21:32:27 -0600

    fixed formats

--- HKEX/fetchData.sh
+++ HKEX/fetchData.sh
@@ -7,13 +7,16 @@ NOW=$(date +"%F")
 cd $SCRIPT_ROOT
 echo "Fetching Data"
 lynx -dump http://www.hkex.com.hk/eng/ddp/Contract_Details.asp?PId=2 | sed -e '1,21d' -e 's/[ \t]*//' > HSI.txt
-lynx -dump http://www.hkex.com.hk/eng/ddp/Contract_Details.asp?PId=6 |
+lynx -dump http://www.hkex.com.hk/eng/ddp/contract_details.asp?pid=6 |
 sed -e '1,21d' -e 's/[ \t]*//' > HHI.txt
+lynx -dump http://www.hkex.com.hk/eng/ddp/contract_details.asp?pid=117 |
+sed -e '1,21d' -e 's/[ \t]*//' > HKB.txt
 
 echo "Data Fetch Completed"
 
 ./format.rb "HSI"
 ./format.rb "HHI"
+./format.rb "HKB"
 
 echo "Finisehd Formatting"
 
@@ -23,6 +26,10 @@ $DROPBOX_UPLOADER -f /home/vincent/.dropbox_uploader mkdir "$NOW" >> ~/log
 $DROPBOX_UPLOADER -f /home/vincent/.dropbox_uploader upload "$SCRIPT_ROOT/HHI.csv" "$NOW/HHI.csv" >> ~/log
 $DROPBOX_UPLOADER -f /home/vincent/.dropbox_uploader upload "$SCRIPT_ROOT/HSI.csv" "$NOW/HSI.csv" >> ~/log
 
+$DROPBOX_UPLOADER -f /home/vincent/.dropbox_uploader upload "$SCRIPT_ROOT/HKB.csv" "$NOW/HKB.csv" >> ~/log
+
 echo "Upload completed, cleaning up"
+#rm *.csv
 rm HHI.*
 rm HSI.*
+rm HKB.*
--- HKEX/format.rb
+++ HKEX/format.rb
@@ -5,15 +5,21 @@ puts "working on " + ARGV[0]
 fh = File.readlines(ARGV[0] + '.txt')
 index_name = fh.first
 data = fh.select { |line| line =~ /^(P||C) / }
-data.each { |line| line.gsub!(" ", ",")} 
+data.each { |line| 
+	line.gsub!(",", "")
+	line.gsub!(" ", ",")
+	line[1] = " "
+	line[8] = " "
+	line[10] = " "
+} 
 
-put_data, call_data = data.partition { |line| line =~ /^P,/}
+put_data, call_data = data.partition { |line| line =~ /^P /}
 
 File.open(ARGV[0] + ".csv", "w") do |file|
 
-	file.write("Contract,Open,Bid,Ask,Last,Traded,High,Low,Volume,Prev.,Day,Settlement,Price,Net,Change,Prev.,Day,Open,Interest\n")
-	file.write call_data.join("\n")
-	file.write put_data.join("\n")
+	file.write("Contract,Open,Bid,Ask,Last,Traded,High,Low,Volume,Prev. DaySettlement Price,Net Change,Prev. Day Open Interest\n")
+	file.write call_data.join("")
+	file.write put_data.join("")
 
 end
 
