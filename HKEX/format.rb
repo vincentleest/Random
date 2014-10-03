8e94edccf92fb66f5b489f3dd72ee03070ea4165
vincent <vincentleest@gmail.com>
2014-10-03 20:45:13 +0800

    Fixed format

--- HKEX/format.rb
+++ HKEX/format.rb
@@ -17,7 +17,7 @@ put_data, call_data = data.partition { |line| line =~ /^P /}
 
 File.open(ARGV[0] + ".csv", "w") do |file|
 
-	file.write("Contract,Open,Bid,Ask,Last,Traded,High,Low,Volume,Prev. DaySettlement Price,Net Change,Prev. Day Open Interest\n")
+	file.write("Contract,Open,Bid,Ask,Last Traded,High,Low,Volume,Prev. DaySettlement Price,Net Change,Prev. Day Open Interest\n")
 	file.write call_data.join("")
 	file.write put_data.join("")
 
