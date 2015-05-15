3013240ad34f86c6d40ab2357aef29879c669ea2
Vincent <vincentleest@gmail.com>
2015-05-15 01:29:08 -0400

    refactored script.

--- HKEX/fetchData.sh
+++ HKEX/fetchData.sh
@@ -5,6 +5,8 @@ DROPBOX_UPLOADER="$SCRIPT_ROOT/dropbox_uploader.sh"
 
 NOW=$(date +"%F")
 HSI_DATE=$(date +"%-d%b%y")
+FILE_NAMES=(HSI HHI HKB TCH HEX CHT XCC)
+ZIP_FILE_NAMES=(hsif hsio hhif hhio stock dqe)
 
 cd $SCRIPT_ROOT
 echo "Fetching Data"
@@ -18,14 +20,10 @@ lynx -dump -width=100 http://www.hkex.com.hk/eng/ddp/contract_details.asp?pid=23
 
 echo "Data Fetch Completed"
 
-./format.rb "HSI"
-./format.rb "HHI"
-./format.rb "HKB"
-./format.rb "TCH"
-./format.rb "HEX"
-./format.rb "CHT"
-./format.rb "XCC"
-
+for i in ${FILE_NAMES[@]}
+do 
+ ./format.rb $i
+done
 echo "Finisehd Formatting"
 
 echo "Uploading files"
@@ -33,19 +31,10 @@ TODAY=$(date +"%y%m%d")
 #moving files around
 $DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" mkdir $NOW 
 
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload "$SCRIPT_ROOT/HHI.csv" "$NOW/HHI$TODAY.csv" 
-
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload "$SCRIPT_ROOT/HSI.csv" "$NOW/HSI$TODAY.csv" 
-
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload "$SCRIPT_ROOT/HKB.csv" "$NOW/HKB$TODAY.csv" 
-
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload "$SCRIPT_ROOT/TCH.csv" "$NOW/TCH$TODAY.csv" 
-
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload "$SCRIPT_ROOT/HEX.csv" "$NOW/HEX$TODAY.csv" 
-
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload "$SCRIPT_ROOT/CHT.csv" "$NOW/CHT$TODAY.csv" 
-
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload "$SCRIPT_ROOT/XCC.csv" "$NOW/XCC$TODAY.csv" 
+for i in ${FILE_NAMES[@]}
+do 
+  $DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload "$SCRIPT_ROOT/$i.csv" "$NOW/$i$TODAY.csv" 
+done
 
 echo "Upload completed, cleaning up"
 rm *.csv
@@ -55,25 +44,18 @@ mv *.txt "archive$TODAY/"
 zip -r "archive$TODAY.zip" "archive$TODAY"
 rm -r "archive$TODAY/"
 
-wget -w 3 -t 3 "https://www.hkex.com.hk/eng/stat/dmstat/dayrpt/hsif$TODAY.zip"
-wget -w 3 -t 3 "https://www.hkex.com.hk/eng/stat/dmstat/dayrpt/hsio$TODAY.zip"
-wget -w 3 -t 3 "https://www.hkex.com.hk/eng/stat/dmstat/dayrpt/hhif$TODAY.zip"
-wget -w 3 -t 3 "https://www.hkex.com.hk/eng/stat/dmstat/dayrpt/hhio$TODAY.zip"
-wget -w 3 -t 3 "https://www.hkex.com.hk/eng/stat/dmstat/dayrpt/stock$TODAY.zip"
-wget -w 3 -t 3 "https://www.hkex.com.hk/eng/stat/dmstat/dayrpt/dqe$TODAY.zip"
+for i in ${ZIP_FILE_NAMES[@]}
+do 
+	wget -w 3 -t 3 "https://www.hkex.com.hk/eng/stat/dmstat/dayrpt/$i$TODAY.zip"
+	$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload "$SCRIPT_ROOT/$i$TODAY.zip" "$NOW/"
+done
+
 wget -w 3 -t 3 "http://www.hsi.com.hk/HSI-Net/static/revamp/contents/en/indexes/report/hsi/HSI_$HSI_DATE.xls"
 wget -w 3 -t 3 "http://www.hsi.com.hk/HSI-Net/static/revamp/contents/en/indexes/report/hscei/HSCEI_$HSI_DATE.xls"
 
-
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload  "$SCRIPT_ROOT/hsif$TODAY.zip" "$NOW/"
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload  "$SCRIPT_ROOT/hsio$TODAY.zip" "$NOW/"
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload  "$SCRIPT_ROOT/hhif$TODAY.zip" "$NOW/"
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload  "$SCRIPT_ROOT/hhio$TODAY.zip" "$NOW/"
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload  "$SCRIPT_ROOT/stock$TODAY.zip" "$NOW/"
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload  "$SCRIPT_ROOT/dqe$TODAY.zip" "$NOW/"
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload  "$SCRIPT_ROOT/archive$TODAY.zip" "$NOW/"
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload  "$SCRIPT_ROOT/HSI_$HSI_DATE.xls" "$NOW/"
-$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload  "$SCRIPT_ROOT/HSCEI_$HSI_DATE.xls" "$NOW/"
+$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload "$SCRIPT_ROOT/archive$TODAY.zip" "$NOW/"
+$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload "$SCRIPT_ROOT/HSI_$HSI_DATE.xls" "$NOW/"
+$DROPBOX_UPLOADER -f "$HOME/.dropbox_uploader" upload "$SCRIPT_ROOT/HSCEI_$HSI_DATE.xls" "$NOW/"
 
 rm *.zip
 rm *.xls
