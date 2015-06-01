283f4f257839501d1be63955d39bfe9209bbbae0
root <root@localhost.localdomain>
2015-06-01 11:35:43 -0400

    added by date feature

--- HKEX/iihk.rb
+++ HKEX/iihk.rb
@@ -9,6 +9,10 @@ def first_monday
 	Date.new(Date.today.year, Date.today.month).step(Date.new(Date.today.year, Date.today.month, -1)).find(&:monday?)
 end
 
+def till_today
+	Date.new(Date.today.year, Date.today.month,1).step(Date.today).reject{|x| [0,6,7].include?(x.wday)}
+end
+
 def new_workbook? 
 	Date.today == first_monday
 end
@@ -24,29 +28,34 @@ def download_file(file_name)
 end
 
 def get_option_data(name)
+    get_option_data_by_date(name, Date.today)
+end
+
+def get_option_data_by_date(name, date)
 	puts "Getting #{name} Option data"
-	option_date = Date.today.strftime("%y%m%d")
-	option_month = Date.today.strftime("%^b-%y")
+	option_date = date.strftime("%y%m%d")
+	option_month = date.strftime("%^b-%y")
     begin    
 	uri = URI('http://iihk.org/hkex/index.php')
 	res = Net::HTTP.post_form(uri, :option_class=> name, :option_date=> option_date, :option_month => option_month, :submitbutton => URI.unescape("%E6%8C%89%E6%AD%A4%E6%9F%A5%E8%A9%A2"))
 	res.body
     rescue Errno::ENETUNREACH
       puts "used curl"
-      data = `curl --data 'option_class=#{name}&option_date=150521&option_month=MAY-15&submitbutton=%E6%8C%89%E6%AD%A4%E6%9F%A5%E8%A9%A2' http://iihk.org/hkex/index.php`
+      data = `curl --data 'option_class=#{name}&option_date=#{option_date}&option_month=#{option_month}&submitbutton=%E6%8C%89%E6%AD%A4%E6%9F%A5%E8%A9%A2' http://iihk.org/hkex/index.php`
     end
 
     unless res.nil? then res.body else data end
 end
 
+
 def option_xlsx_filename(option)
 	"OptionStatus-#{option.upcase}-#{Date.today.year}-#{Date.today.strftime("%^b")}.xlsx"
 end
 
-def save_to_workbook(name, data)
+def save_to_workbook(name, data, date)
 	puts "Saving #{name} option data to workbook"
 	file_name = option_xlsx_filename(name)
-	sheet_name = Date.today.strftime("%m-%d")
+	sheet_name = date.strftime("%m-%d")
 
 	if new_workbook?
 		workbook = RubyXL::Workbook.new(file_name)
@@ -79,9 +88,22 @@ def save_to_workbook(name, data)
 	workbook.write(file_name)
 end
 
+#till_today.each{ |d|
+#  
+#  data = get_option_data_by_date( :hsi, d)
+#  save_to_workbook(:hsi, data, d)
+#
+#  data = get_option_data_by_date( :hhi, d)
+#  save_to_workbook(:hhi, data, d)
+#}
+#
+#["hsi", "hhi"].each { |o|
+#  upload_file option_xlsx_filename o    
+#}
+#
 ["hsi", "hhi"].each { |o|
   data = get_option_data o
-  save_to_workbook(o, data)
+  save_to_workbook(o, data, Date.today)
   upload_file option_xlsx_filename o    
   File.delete option_xlsx_filename o
 }
