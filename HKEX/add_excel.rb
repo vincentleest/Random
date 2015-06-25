2f589aef08780806c813df65f30d8b5bc0b1438b
Vincent <vincentleest@gmail.com>
2015-06-24 21:34:58 -0400

    Fixed formatting issue for iihk

--- HKEX/add_excel.rb
+++ HKEX/add_excel.rb
@@ -29,27 +29,36 @@ require 'nokogiri'
 #
 #workbook.write "./test.xlsx"
 
-data = File.read "./data"
+data = File.read "./data.html"
 
 
+
+
+lines = Array.new
 doc = Nokogiri::HTML(data)
-#doc.xpath('//table//* [self::tr or self::th]').each_with_index do |row, i|
-doc.xpath('//table//thead//tr').each_with_index do |row, i|
-  puts "row #{i}"
-  puts row
-  #puts row.text.gsub("\n", ' ').gsub('"', '\"').gsub(/(\s){2,}/m, '\1')
-  row.xpath('th').each_with_index do |cell,j|
-      puts "th #{j}"
-	  puts cell.text
-  end
+doc.xpath('//table//tr').each_with_index do |row, i|
+	line = ""
+    row.xpath('th').each do |cell|
+      #tmp ='"', cell.text.gsub("\n", '').gsub('"', '\"').gsub(/(\s){2,}/m, '\1'), "\","
+      tmp =cell.text.gsub("\n", '').gsub('"', '\"').gsub(/(\s){2,}/m, '\1'), ","
+	  line << tmp.join('')
+    end
+    row.xpath('td').each do |cell|
+      #tmp ='"', cell.text.gsub("\n", '').gsub('"', '\"').gsub(/(\s){2,}/m, '\1'), "\","
+      tmp =cell.text.gsub("\n", '').gsub('"', '\"').gsub(/(\s){2,}/m, '\1'), ","
+	  line << tmp.join('')
+    end
+	lines.push line
 end
 
+
+#lines.each { |x| puts x}
+lines[-1].split(',').each { |cell| puts cell}
 
 #puts footer values
 #doc = Nokogiri::HTML(data)
 ##doc.xpath('//table//* [self::tr or self::th]').each_with_index do |row, i|
 #doc.xpath('//table//tfoot//tr').each_with_index do |row, i|
-#  puts row.methods
 #  #puts row.text.gsub("\n", ' ').gsub('"', '\"').gsub(/(\s){2,}/m, '\1')
 #  row.xpath('th').each_with_index do |cell,j|
 #	  puts cell.text
