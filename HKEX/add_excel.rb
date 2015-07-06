22a6fc97591d9b49aa92d0c5b1b761b7af7e9229
root <root@localhost.localdomain>
2015-07-06 11:03:17 -0400

    added random code

--- HKEX/add_excel.rb
+++ HKEX/add_excel.rb
@@ -1,17 +1,23 @@
 #!/usr/bin/env ruby
 
-require 'rubyXL'
-
+#require 'rubyXL'
+require 'date'
 
 #workbook = RubyXL::Parser.parse '/Users/vincentleest/Downloads/HKEX-HSIOptions-2015-Master.xlsx'
 
-workbook = RubyXL::Workbook.new
+#workbook = RubyXL::Workbook.new
+#
+#puts workbook.worksheets.size
+#
+#ws = workbook[0]
+#
+#ws.add_cell(0,0, 'asdf')
+#
+#workbook.write "./test.xlsx"
 
-puts workbook.worksheets.size
+year =  Date.today.year
+puts year
+last_day = (Date.new(year, 1)..Date.new(year+1, 1)).select {|d| d.day == 1}.map {|d| d - 1}.drop(1)
 
-ws = workbook[0]
-
-ws.add_cell(0,0, 'asdf')
-
-workbook.write "./test.xlsx"
-
+puts last_day.nil?
+puts last_day.join " "
