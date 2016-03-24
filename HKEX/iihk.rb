87a82215ed4c7d2a6c69d95138161428c09aee6f
root <root@localhost.localdomain>
2016-03-24 19:55:03 -0400

    Added feature to respect option closing/opting dates. Fixes issue #1

--- HKEX/iihk.rb
+++ HKEX/iihk.rb
@@ -9,12 +9,34 @@ def first_monday
 	Date.new(Date.today.year, Date.today.month).step(Date.new(Date.today.year, Date.today.month, -1)).find(&:monday?)
 end
 
-def till_today
-	Date.new(Date.today.year, Date.today.month,1).step(Date.today).reject{|x| [0,6,7].include?(x.wday)}
+def expiry_day month
+    days = Date.new(Date.today.year, month,1).step(Date.new(Date.today.year, month, -1)).reject{|x| [0,6,7].include?(x.wday)} 
+    days[-2]
 end
 
-def new_workbook? 
-	Date.today == first_monday
+def expiry_day? day
+    day == expiry_day(day.month)
+end
+
+def till_today
+    expiry_day(Date.today.month-1).step(Date.today).reject{|x| [0,6,7].include?(x.wday)}
+end
+
+def periods day
+    periods = Array.new
+    if day < expiry_day(day.month)
+        periods.push(day.strftime("%^b-%y")) 
+    elsif expiry_day? day 
+        periods.push(day.strftime("%^b-%y")) 
+        periods.push(day.next_month.strftime("%^b-%y")) 
+    else
+        periods.push(day.next_month.strftime("%^b-%y")) 
+    end
+    periods
+end
+
+def new_workbook?(day, period)
+    expiry_day? day and day.next_month.month == Date.parse(period).month
 end
 
 def full_path file_name
@@ -28,35 +50,14 @@ end
 
 def download_file(file_name)
 	home = ENV['HOME']
-  command = "#{home}/workspace/Random/HKEX/dropbox_uploader.sh -f '#{home}/.dropbox_uploader' download '#{file_name}' '#{full_path file_name}'"
-  puts command
+    command = "#{home}/workspace/Random/HKEX/dropbox_uploader.sh -f '#{home}/.dropbox_uploader' download '#{file_name}' '#{full_path file_name}'"
 	system command
 end
 
-def get_option_data(name)
-    get_option_data_by_date(name, Date.today)
-end
-
-def get_option_data_by_date(name, date)
-	puts "Getting #{name} Option data"
+def get_option_data_by_date(name, date, option_month=nil)
+	puts "Getting "+name+" Option data"
 	option_date = date.strftime("%y%m%d")
-	option_month = date.strftime("%^b-%y")
-    begin    
-	uri = URI('http://iihk.org/hkex/index.php')
-	res = Net::HTTP.post_form(uri, :option_class=> name, :option_date=> option_date, :option_month => option_month, :submitbutton => URI.unescape("%E6%8C%89%E6%AD%A4%E6%9F%A5%E8%A9%A2"))
-	res.body
-    rescue Errno::ENETUNREACH
-      puts "used curl"
-      data = `curl --data 'option_class=#{name}&option_date=#{option_date}&option_month=#{option_month}&submitbutton=%E6%8C%89%E6%AD%A4%E6%9F%A5%E8%A9%A2' http://iihk.org/hkex/index.php`
-    end
-
-    unless res.nil? then res.body else data end
-end
-
-def get_option_data_by_date_and_month(name, date, month)
-	puts "Getting #{name} Option data"
-	option_date = date.strftime("%y%m%d")
-	option_month = date.strftime("%^b-%y")
+    option_month ||= date.strftime("%^b-%y")
 
     begin    
 	uri = URI('http://iihk.org/hkex/index.php')
@@ -70,17 +71,16 @@ def get_option_data_by_date_and_month(name, date, month)
     unless res.nil? then res.body else data end
 end
 
-
-def option_xlsx_filename(option)
-	"OptionStatus-#{option.upcase}-#{Date.today.year}-#{Date.today.strftime("%^b")}.xlsx"
+def option_xlsx_filename(option, period)
+	"OptionStatus-#{option.upcase}-#{Date.today.year}-#{period}.xlsx"
 end
 
-def save_to_workbook(name, data, date)
+def save_to_workbook(name, data, date, period)
 	puts "Saving #{name} option data to workbook"
-	file_name = option_xlsx_filename(name)
+	file_name = option_xlsx_filename(name, period)
 	sheet_name = date.strftime("%m-%d")
 
-	if new_workbook?
+	if new_workbook?(date, period)
 		workbook = RubyXL::Workbook.new
 		ws = workbook["Sheet1"]
 		ws.sheet_name = sheet_name
@@ -169,10 +169,10 @@ end
 #
 #till_today.each{ |d|
 #  
-#  data = get_option_data_by_date( :hsi, d)
+#  data = get_option_data_by_date(:hsi, d)
 #  save_to_workbook(:hsi, data, d)
 #
-#  data = get_option_data_by_date( :hhi, d)
+#  data = get_option_data_by_date(:hhi, d)
 #  save_to_workbook(:hhi, data, d)
 #}
 
@@ -189,10 +189,24 @@ end
 #download_file(file_name)	
 
 #puts "CWD is: #{system 'pwd'}"
-["hsi", "hhi"].each { |o|
-  Date.today.strftime("%y%m%d")
-  data = get_option_data o
-  save_to_workbook(o, data, Date.today)
-  upload_file option_xlsx_filename o    
-  File.delete(full_path option_xlsx_filename o)
-}
+["hsi", "hhi"].each do |o|
+  periods(Date.today).each do |p|
+    data = get_option_data_by_date(o, Date.today, p)
+    save_to_workbook(o, data, Date.today, p)
+    file_name = option_xlsx_filename(o, p)
+    upload_file file_name
+    File.delete(full_path file_name)
+  end
+end
+   
+#Date.new(2015,12,29).step(Date.new(2016,12,31)).each do |d|
+#  if expiry_day? d
+#    puts "#{d} is an expiry day"
+#  end
+#  puts  "#{d} has periods #{periods(d)}"
+#    
+##    puts "Should create new workbook for period #{d.month} on #{d}!" if new_workbook?(d,d.month)
+##    puts "Should create new workbook for period #{d.next_month.month} on #{d}!" if new_workbook?(d,d.next_month.month)
+#end
+
+
